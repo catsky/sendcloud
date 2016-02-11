3d283474b2f974ad10a555d19356f369901bf053
catsky <ken@enquire.io>
2016-02-11 15:51:17 +1000

    gitignore

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rspec'
-require 'sendcloud'
-
-RSpec.configure do |_|
-  Sendcloud.setup do |config|
-    config.api_user = 'mail_chilaoshi'
-    config.api_key = 'dvB7ft2bdUavgYMW'
-  end
-end
